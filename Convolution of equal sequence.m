84fdda2add7289e1148d203ee5de24a948132c5d
Zain Anwar Rajani <zainrajani2207@gmail.com>
2019-09-24 22:11:14 -0400

    Create Convolution of equal sequence.m

---
+++ Convolution of equal sequence.m
@@ -0,0 +1,40 @@
+%Convolution of Equal Sequence length 
+clear all;
+close all;
+clc;
+n=0:1:5;
+x=[1 4 2 2 1 3];
+h=[4 2 1 3 4 1];
+figure
+ 
+%Plot x[n]
+subplot(2,2,1)
+stem(n,x);
+title('x[n]');
+xlabel('n');
+ylabel ('Amplitude');
+ 
+%Plot h[n]
+subplot(2,2,2)
+stem(n,h);
+xlabel (' n');
+ylabel('Amplitude');
+title('h[n]')
+ 
+%Linear convolution of signal
+y=conv(x,h);
+ 
+%plotting convoluted signal
+subplot(2,2,3);
+stem(y)
+xlabel('n');
+ylabel('Amplitude')
+title('Convolution Signal x[n]*h[n]');
+ 
+%Circular convolution
+z=cconv(x,h);
+subplot(2,2,4);
+stem(z);
+xlabel('n');
+ylabel('Amplitude');
+title('Circular convolution of x[n]*h[n]');
